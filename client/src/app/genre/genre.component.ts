c9a78b4197ca02d64bc7af28525bcce63e8dadb6
employee <vadymz@softwareplanet.uk.com>
2016-12-13 15:40:33 +0200

    change component for genre

--- client/src/app/genre/genre.component.ts
+++ client/src/app/genre/genre.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
-import { Genre } from './genre';
-import { GenreService } from './genre.service';
+import {Genre} from './genre';
+import {GenreService} from './genre.service';
 
 @Component({
   moduleId: module.id.toString(),
   selector: 'my-genres',
   templateUrl: 'genre.component.html',
-  styleUrls: [ 'genre.component.css' ]
+  styleUrls: ['genre.component.css']
 })
 export class GenreComponent implements OnInit {
   genres: Genre[];
   selectedGenre: Genre;
-  selectedView : boolean;
+  selectedView: boolean;
 
-  constructor(
-    private router: Router,
-    private genreService: GenreService) { }
+  constructor(private router: Router,
+              private genreService: GenreService) {
+  }
 
   getGenres(): void {
     this.genreService.getGenres().then(genre => this.genres = genre);
@@ -30,24 +30,31 @@ export class GenreComponent implements OnInit {
   onSelect(genre: Genre): void {
     this.selectedGenre = genre;
   }
-  view(view: boolean):void{
-    this.selectedView=!view;
+
+  view(view: boolean): void {
+    this.selectedView = !view;
   }
+
   add(name: string): void {
     name = name.trim();
-    if (!name) { return; }
+    if (!name) {
+      return;
+    }
     this.genreService.create(name)
       .then(genre => {
         this.genres.push(genre);
         this.selectedGenre = null;
       });
   }
-  delete(genre : Genre): void {
+
+  delete(genre: Genre): void {
     this.genreService
       .delete(genre.id)
       .then(() => {
         this.genres = this.genres.filter(h => h !== genre);
-        if (this.selectedGenre === genre) { this.selectedGenre = null; }
+        if (this.selectedGenre === genre) {
+          this.selectedGenre = null;
+        }
       });
   }
 }
